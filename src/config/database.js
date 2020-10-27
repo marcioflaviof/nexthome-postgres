d9ad83bbc230af1ce6581f5fac5a9ccc61c88359
Marcio Flavio <mflaviof1995@gmail.com>
2020-10-27 12:23:19 -0300

    Testing heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,30 +1,32 @@
 require('dotenv').config()
 
-//  if (process.env.DATABASE_URL) {
-    // module.exports = {
-    //     dialect: process.env.DIALECT,
-    //     protocol: process.env.PROTOCOL,
-    //     host: process.env.DB_H_HOST,
-    //     port: process.env.DB_H_PORT,
-    //     database: process.env.DB_NAME,
-    //     SSL: true,
-    //     define: {
-    //         timestamps: true,
-    //         underscored: true
-    //     }
-    
-    // }
-//  } else {
+
     module.exports = {
         dialect: process.env.DIALECT,
-        host: process.env.DB_HOST,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASS,
+        protocol: process.env.PROTOCOL,
+        host: process.env.DB_H_HOST,
+        port: process.env.DB_H_PORT,
         database: process.env.DB_NAME,
+        ssl: {
+            rejectUnauthorized: false
+        },
         define: {
             timestamps: true,
             underscored: true
         }
-
+    
     }
-//  }
+
+    // module.exports = {
+    //     dialect: process.env.DIALECT,
+    //     host: process.env.DB_HOST,
+    //     username: process.env.DB_USERNAME,
+    //     password: process.env.DB_PASS,
+    //     database: process.env.DB_NAME,
+    //     define: {
+    //         timestamps: true,
+    //         underscored: true
+    //     }
+
+    // }
+ 

2e1cab727544f44caa62ae5271bb2bde6e8b5422
Marcio Flavio <mflaviof1995@gmail.com>
2020-10-27 12:45:52 -0300

    Adjusting database

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ require('dotenv').config()
         dialect: process.env.DIALECT,
         host: process.env.DB_H_HOST,
         port: process.env.DB_H_PORT,
-        database: process.env.DB_NAME,
+        database: process.env.DB_H_DATABASE,
         username: process.env.DB_H_USERNAME,
         password: process.env.DB_H_PASS,
         ssl: {

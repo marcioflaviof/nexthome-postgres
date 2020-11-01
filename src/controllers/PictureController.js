27378e06f0cda12dd77b94044e1c11ea3620ce02
Marcio Flavio <mflaviof1995@gmail.com>
2020-10-31 21:20:31 -0300

    User or house requeriment to create a image

--- src/controllers/PictureController.js
+++ src/controllers/PictureController.js
@@ -10,12 +10,20 @@ module.exports = {
         const { originalname: name, size, filename: key} = req.file
     
         const { user_id, house_id } = req.params
+
+        const user = await User.findByPk(user_id)
+        const house = await House.findByPk(house_id)
+
+        if (!user && !house) {
+            return res.status(400).json({ err:"It's necessary one user or house" })
+        } else {
     
-        try {
-            const picture = await Picture.create({ user_id, house_id, name, size, key, url:'' })
-            return res.json(picture)
-        } catch (error) {
-            return res.status(400).json({ err: error.message })
+            try {
+                const picture = await Picture.create({ user_id, house_id, name, size, key, url:'' })
+                return res.json(picture)
+            } catch (error) {
+                return res.status(400).json({ err: error.message })
+            }
         }
         
     }
